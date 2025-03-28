009414a3aec4745a4efa50c7e7edb229c440ed97
Volodymyr Fufalko <devfinkord@gmail.com>
2025-03-29 01:21:23 +0200

    coverage below 70%

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,6 +4,6 @@ def test_add():
     """Tests the addition function."""
     assert add(2,3) == 5
 
-def test_subtract():
-    """Tests the substraction functiion."""
-    assert subtract(5,3) == 2
+# def test_subtract():
+#     """Tests the substraction functiion."""
+#     assert subtract(5,3) == 2
